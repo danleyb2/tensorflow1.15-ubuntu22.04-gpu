59fb22ba191a17d0310c853219f3b02ca5939d60
nyaundi brian <danleyb2@gmail.com>
2023-01-08 18:28:37 +0300

    w

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-# Git
-apt-get install git -y
+apt-get update
+
+apt install wget zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev build-essential git -y
+
 
 # Install Python3.7 using install-python3.7.sh
 ./install-python3.7.sh
--- install-python3.7.sh
+++ install-python3.7.sh
@@ -1,8 +1,6 @@
-apt-get update
 
 cd /tmp/
 
-apt install wget zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev build-essential -y
 wget https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
 
 apt-get install xz-utils
@@ -22,4 +20,4 @@ make install
 
 ldconfig /usr/local/lib 
 
-cd .. && rm -r Python-3.7.13/
+# cd .. && rm -r Python-3.7.13/
