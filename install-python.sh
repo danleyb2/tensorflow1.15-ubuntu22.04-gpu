8718a0bdb5e3932a86071f0e6e17fbe142b4b6ed
nyaundi brian <danleyb2@gmail.com>
2023-01-08 19:15:41 +0300

    w

--- install-python.sh
+++ install-python.sh
@@ -10,27 +10,24 @@ apt -y install zlib1g-dev \
     libffi-dev \
     libbz2-dev
 
-cd /tmp/
-
 apt -y install wget build-essential 
-
-wget -O /tmp/Python-3.7.13.tar.xz https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
-
-apt-get install xz-utils
+wget -O Python-3.7.13.tar.xz https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
 tar -xf Python-3.7.13.tar.xz
 
 cd Python-3.7.13/
 
 ./configure --enable-optimizations --enable-shared
 
-# With tests. Recomended
+# With tests and optimizations. Recomended
 # make -j8 
 
 # Skip tests
 make -j8 build_all
 
+# Install
 make install
 
 ldconfig /usr/local/lib 
 
-# cd .. && rm -r Python-3.7.13/
+cd -
+rm -r Python-3.7.13/
