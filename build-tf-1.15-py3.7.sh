dd208a5d2dd0b9e2d75a5d712a14cb8722d164ea
nyaundi brian <danleyb2@gmail.com>
2023-01-08 17:45:03 +0300

    initial commit

---
+++ build-tf-1.15-py3.7.sh
@@ -0,0 +1,22 @@
+# Git
+apt-get install git -y
+
+# Install Python3.7 using install-python3.7.sh
+./install-python3.7.sh
+
+
+# Clone Tensorflow
+git clone https://github.com/tensorflow/tensorflow
+
+# checkout v1.15.5
+cd tensorflow
+git checkout v1.15.5
+
+
+
+
+
+
+
+
+
