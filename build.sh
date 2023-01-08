2794f9cad0096a153722da09eede1d8098e933ff
nyaundi brian <danleyb2@gmail.com>
2023-01-08 19:06:19 +0300

    w

--- build.sh
+++ build.sh
@@ -5,22 +5,18 @@ set -o pipefail
 
 apt-get update
 
-
 # Install Python3.7 
-./install-python.sh
-
+$PWD/install-python.sh
 
 # Install Bazel 
-./install-bazel.sh
+$PWD/install-bazel.sh
 
 apt install -y git
+
 # Clone Tensorflow
 git clone https://github.com/tensorflow/tensorflow
 
 # checkout v1.15.5
 cd tensorflow
 git checkout v1.15.5
-
-
-
 
